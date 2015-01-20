8d8c5f51d7467bb271711b0849aba57f5fce1fff
Baiguang <alex987965@gmail.com>
2015-01-20 23:34:48 +0800

    ExpandListViewAdapter适配器增加必要字段

--- src/com/expandable/listview/adapter/ExpandListViewAdapter.java
+++ src/com/expandable/listview/adapter/ExpandListViewAdapter.java
@@ -1,9 +1,13 @@
 package com.expandable.listview.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import com.expandable.listview.bean.Node;
+
+import java.util.List;
 
 /**
  * Created by Baiguang on 2015/1/19.
@@ -13,6 +17,22 @@ import android.widget.BaseAdapter;
 public class ExpandListViewAdapter<T> extends BaseAdapter {
 
     protected Context mContext;
+
+    protected List<Node> mVisibleNodes;
+
+    protected LayoutInflater mInflater;
+
+    protected List<Node> mAllNodes;
+
+    public interface OnExpandNodeClickListener {
+        void onClick(Node node, List<Node> visibleNodes, View view, int position);
+    }
+
+    private OnExpandNodeClickListener onExpandNodeClickListener;
+
+    public void setOnExpandNodeClickListener(OnExpandNodeClickListener listener){
+        this.onExpandNodeClickListener = listener;
+    }
 
     @Override
     public int getCount() {
